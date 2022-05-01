bad6b3cff9bc5e45c3beb9e4d5554624e216c0ec
Julian Bull <julianbullmagic@gmail.com>
2022-05-02 00:09:29 +1000

    fff

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,103 @@
+<?php return array (
+  'bagusindrayana/laravel-maps' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Bagusindrayana\\LaravelMaps\\LaravelMapsServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravel/ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Ui\\UiServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'larswiegers/laravel-maps' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Larswiegers\\LaravelMaps\\LaravelMapsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'LaravelMaps' => 'Larswiegers\\LaravelMaps\\LaravelMapsFacade',
+    ),
+  ),
+  'mastani/laravel-google-static-map' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mastani\\GoogleStaticMap\\GoogleStaticMapServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'GoogleStaticMap' => 'Mastani\\GoogleStaticMap\\GoogleStaticMapFacade',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'siberfx/backpack-leafletjs' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Siberfx\\Leafletjs\\ServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+  ),
+);
