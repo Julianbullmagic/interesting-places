7c8e2e37bc0b93aae41e82d6c3bba791c6ca2276
Julian Bull <julianbullmagic@gmail.com>
2022-05-02 09:34:37 +1000

    fff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,9 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      Schema::defaultStringLength(191);
-      if (App::environment('production')) {
-          URL::forceScheme('https');
-      }             
+      Schema::defaultStringLength(191);           
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    <script src="{{asset('js/app.js')}}"></script>
+    <link href="css/app.css" rel="stylesheet">
+    <script src="js/app.js"></script>
 
 </head>
 <body>
